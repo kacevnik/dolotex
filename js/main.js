8aff1071167d602db85402b727ee4d8c55b5a15c
kacevnik <kacevnik@yandex.ru>
2018-04-21 23:34:23 +0300

    Верстка Section #2,Section#3 Главной страницы

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
    $(document).ready(function() {
-      $('#mmenu').mmenu({
-         navbar:{
+      $("#mmenu").mmenu({
+         navbar: {
             title: "DOLOTEX"
          }
       });
 
-      var dataMmenu = $('#mmenu').data("mmenu");
-
-      dataMmenu.bind('open:finish', function(){
-         $('.hamburger').addClass('is-active');
-      }).bind('close:finish', function(){
-         $('.hamburger').removeClass('is-active');
-      });
-
+       var api = $("#mmenu").data( "mmenu" );
+       api.bind('open:finish', function() {
+          $('.hamburger').addClass('is-active');
+       }).bind('close:finish', function(){
+            $('.hamburger').removeClass('is-active');
+          });
    });
