84d54444bd7513bbc7f11867504cc47f827f1d33
kacevnik <kacevnik@yandex.ru>
2018-04-21 09:02:20 +0300

    Подключение Mmenu, hamburger. Адаптивность Header

---
+++ js/main.js
@@ -0,0 +1,16 @@
+   $(document).ready(function() {
+      $('#mmenu').mmenu({
+         navbar:{
+            title: "DOLOTEX"
+         }
+      });
+
+      var dataMmenu = $('#mmenu').data("mmenu");
+
+      dataMmenu.bind('open:finish', function(){
+         $('.hamburger').addClass('is-active');
+      }).bind('close:finish', function(){
+         $('.hamburger').removeClass('is-active');
+      });
+
+   });
