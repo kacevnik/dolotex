85081a82e5271c78065c582a92c7dade58bf143e
kacevnik <kacevnik@yandex.ru>
2018-05-27 10:01:04 +0300

    Закончил страничку карточки товара

--- single.php
+++ single.php
@@ -13,6 +13,8 @@
         $kdv_on_line_dolg       = fw_get_db_settings_option('kdv_on_line_dolg');
         $kdv_on_line_img        = fw_get_db_settings_option('kdv_on_line_img');
         $kdv_on_line_text       = fw_get_db_settings_option('kdv_on_line_text');
+        $kdv_phone_header       = fw_get_db_settings_option('kdv_phone_header');
+
 
         $arr_kdv_tovar_more     = array();
 
@@ -158,49 +160,51 @@
         </div>
     </section>
     <?php } ?>
-      <section id="product_answ" class="pt_55 pb_65">
+    <section id="product_answ" class="pt_55 pb_65">
         <div class="container">
-          <h2>Остались вопросы?</h2>
-          <div class="form">
-            <div class="row">
-              <h3>Мы свяжемся с Вами в течении 15 минут</h3>
-              <div class="col-md-3">
-                <div class="form_input">
-                  <div class="form_input_icon"><i class="fas fa-user"></i></div>
-                  <div class="form_input_text"><input type="text" name="email" placeholder="Ваше имя" required=""></div>
+            <h2>Остались вопросы?</h2>
+            <form action="<?php echo get_template_directory_uri(); ?>/send.php" method="post">
+                <div class="form">
+                    <div class="row">
+                        <h3>Мы свяжемся с Вами в течении 15 минут</h3>
+                        <div class="col-md-3">
+                            <div class="form_input">
+                                <div class="form_input_icon"><i class="fas fa-user"></i></div>
+                                <div class="form_input_text"><input type="text" name="name" placeholder="Ваше имя" required="" autocomplete="off"></div>
+                                <input type="hidden" name="form" value="Форма: Остались вопросы? ">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form_input">
+                                <div class="form_input_icon"><i class="far fa-envelope"></i></div>
+                                <div class="form_input_text"><input type="text" name="email" placeholder="Ваша почта" required="" autocomplete="off"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form_input">
+                                <div class="form_input_icon"><i class="fas fa-mobile-alt"></i></div>
+                                <input type="hidden" name="name_product" value="<?php the_title(); ?>">
+                                <div class="form_input_text"><input type="text" name="phone" placeholder="+380 (__) __ __ ___" required="" autocomplete="off"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <input type="submit" name="submit" value="Задать вопрос" class="btn">
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div class="col-md-3">
-                <div class="form_input">
-                  <div class="form_input_icon"><i class="far fa-envelope"></i></div>
-                  <div class="form_input_text"><input type="text" name="email" placeholder="Ваша почта" required=""></div>
+            </form>
+            <div class="row article_phone_text">
+                <div class="col-md-8 pnone_text bold300">
+                    Или задайте вопрос эксперту по телефону:
                 </div>
-              </div>
-              <div class="col-md-3">
-                <div class="form_input">
-                <div class="form_input_icon"><i class="fas fa-mobile-alt"></i></div>
-                <input type="hidden" name="name_product">
-                <div class="form_input_text"><input type="text" name="phone" placeholder="+380 (__) __ __ ___" required=""></div>
-              </div>
+                <div class="col-md-4">
+                    <div class="number_phone">
+                        <i class="fas fa-phone"></i><?php echo $kdv_phone_header; ?>
+                    </div>
+                    <div class="phone">
+                        Звонок БЕСПЛАТНЫЙ
+                    </div>
+                </div>
             </div>
-              <div class="col-md-3">
-                <a href="" class="btn">Задать вопрос</a>
-              </div>
-          </div>
-        </div>
-        <div class="row article_phone_text">
-          <div class="col-md-8 pnone_text bold300">
-            Или задайте вопрос эксперту по телефону:
-          </div>
-          <div class="col-md-4">
-            <div class="number_phone">
-              <i class="fas fa-phone"></i>8 800 800 80 80
-            </div>
-            <div class="phone">
-              Звонок БЕСПЛАТНЫЙ
-            </div>
-          </div>
-        </div>
       </section>
-<section>
 <?php get_footer(); ?>
